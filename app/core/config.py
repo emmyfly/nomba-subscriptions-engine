8be7ad22e448dc5103a1307d1b47bfde2fef1768
emmyfly <eakerele71@gmail.com>
2026-06-24 02:17:34 +0100

    Working plans CRUD with database - first endpoint tested

--- app/core/config.py
+++ app/core/config.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 class Settings:
     NOMBA_CLIENT_ID:str = os.getenv("NOMBA_CLIENT_ID","")
-    NOMBA_CLIENT_SECRET: str = os.getenv("NOMBA_CLINT_SECRET","")
+    NOMBA_CLIENT_SECRET: str = os.getenv("NOMBA_CLIENT_SECRET","")
     NOMBA_ACCOUNT_ID: str = os.getenv("NOMBA_ACCOUNT_ID", "")
     NOMBA_BASE_URL: str = os.getenv("NOMBA_BASE_URL","https://sandbox.nomba.com")
 
     DATABASE_URL:str = os.getenv("DATABASE_URL","sqlite:/// ./subscription.db")
     SECRET_KEY: str = os.getenv("SECRET_KEY","dev-secret-change-me")
 
-    Settings = Settings()
+settings = Settings()
